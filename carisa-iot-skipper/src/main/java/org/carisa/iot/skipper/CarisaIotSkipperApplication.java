a3c9cb3fe3b80122bdfebcdffb9953f713c9599c
David Suarez <dav.sua.pas@gmail.com>
2019-02-18 16:54:16 +0100

    Configure skiper references

--- carisa-iot-skipper/src/main/java/org/carisa/iot/skipper/CarisaIotSkipperApplication.java
+++ carisa-iot-skipper/src/main/java/org/carisa/iot/skipper/CarisaIotSkipperApplication.java
@@ -2,13 +2,29 @@ package org.carisa.iot.skipper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.cloud.deployer.spi.kubernetes.KubernetesAutoConfiguration;
+import org.springframework.cloud.deployer.spi.local.LocalDeployerAutoConfiguration;
+import org.springframework.cloud.skipper.server.EnableSkipperServer;
 
-@SpringBootApplication
+/**
+ * Runs the Skipper Server Application.
+ *
+ * @author David Suárez
+ *
+ */
+@SpringBootApplication(exclude = {
+		KubernetesAutoConfiguration.class,
+		LocalDeployerAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		SessionAutoConfiguration.class
+})
+@EnableSkipperServer
 public class CarisaIotSkipperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarisaIotSkipperApplication.class, args);
 	}
-
 }
 
