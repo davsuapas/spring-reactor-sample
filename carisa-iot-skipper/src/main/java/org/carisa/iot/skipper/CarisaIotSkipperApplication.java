6081cf5175c2dd495dd84d9491eea27489c55bd2
David Suarez <dav.sua.pas@gmail.com>
2019-02-18 10:46:46 +0100

    Make project

---
+++ carisa-iot-skipper/src/main/java/org/carisa/iot/skipper/CarisaIotSkipperApplication.java
@@ -0,0 +1,14 @@
+package org.carisa.iot.skipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarisaIotSkipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarisaIotSkipperApplication.class, args);
+	}
+
+}
+
