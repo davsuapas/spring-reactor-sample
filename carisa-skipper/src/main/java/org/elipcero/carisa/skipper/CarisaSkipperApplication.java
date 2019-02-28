cb6f58f474f5afdaba0ddd2e56ea8a8bc7ba59d6
David Suarez <dav.sua.pas@gmail.com>
2019-02-28 16:19:46 +0100

    rename

---
+++ carisa-skipper/src/main/java/org/elipcero/carisa/skipper/CarisaSkipperApplication.java
@@ -0,0 +1,32 @@
+package org.elipcero.carisa.skipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.cloud.deployer.spi.cloudfoundry.CloudFoundryDeployerAutoConfiguration;
+import org.springframework.cloud.deployer.spi.kubernetes.KubernetesAutoConfiguration;
+import org.springframework.cloud.deployer.spi.local.LocalDeployerAutoConfiguration;
+import org.springframework.cloud.skipper.server.EnableSkipperServer;
+
+/**
+ * Runs the Skipper Server Application.
+ *
+ * @author David Suárez
+ *
+ */
+@SpringBootApplication(exclude = {
+		CloudFoundryDeployerAutoConfiguration.class,
+		KubernetesAutoConfiguration.class,
+		LocalDeployerAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		SessionAutoConfiguration.class
+})
+@EnableSkipperServer
+public class CarisaSkipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarisaSkipperApplication.class, args);
+	}
+}
+
