01b3729fe864294ca9e7a59f6791d620b420a335
David Suarez <dav.sua.pas@gmail.com>
2019-04-10 12:51:03 +0200

    Creating carisa administration project

---
+++ carisa-administration/src/main/java/org/elipcero/carisa/administration/CarisaAdministrationApplication.java
@@ -0,0 +1,13 @@
+package org.elipcero.carisa.administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarisaAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarisaAdministrationApplication.class, args);
+	}
+
+}
---
+++ carisa-administration/src/test/java/org/elipcero/carisa/carisaadministration/CarisaAdministrationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.elipcero.carisa.carisaadministration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarisaAdministrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
